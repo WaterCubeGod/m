7cd3668649d1452b4311bc404bbfb2b5494aaf5d
WaterCube <3166474860@qq.com>
2023-11-03 11:21:11 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import My from '../components/routerComponents/My'
 import Team from '../components/routerComponents/Team'
 import Donation from '../components/routerComponents/Donation'
 import Study from '../components/routerComponents/Study'
-import Donation from '../components/routerComponents/Donation'
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
@@ -29,7 +28,6 @@ const router = new VueRouter({
                 { path: '/main/team', name: 'team', component: Team },
                 { path: '/main/donation', name: 'donation', component: Donation },
                 { path: '/main/study', name: 'study', component: Study },
-                { path: '/main/donation', name: 'donation', component: Donation },
             ]
         },
         { path: '/', redirect: '/main' },
